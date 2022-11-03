b34ea671bee95df58c73b546ac9804a5bb45dde7
KIMHEEJIN <gmlwls768@naver.com>
2022-11-04 00:20:35 +0900

    Day 1 practice

---
+++ Day_1/Day1_simple.py
@@ -0,0 +1,18 @@
+print("hello world")
+# output string
+print(input("input your name"))
+# input stirng & print
+print(len(input("input your name")))
+# input stirng & print length
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
+# use valuable
+print("Welcome to the Band Name Generator. \n")
+City = input("What's name of the city you grew up in? \n")
+Pet = input("what's your pet name \n")
+print("Your band name could be "+City + " " + Pet)
