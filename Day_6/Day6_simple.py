5deccb3172aa25e605ebfb3a360cb9252c968e7c
KIMHEEJIN <gmlwls768@naver.com>
2022-11-09 19:21:53 +0900

    Day 6 practice

---
+++ Day_6/Day6_simple.py
@@ -0,0 +1,75 @@
+def my_func():
+    print("Hello")
+    print("Bye")
+
+
+my_func()
+# simple function define and use
+
+
+# #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+# hurdle 2
+# # def turn_right():
+# #     for i in range(0,3):
+# #         turn_left()
+# # def jump():
+# #     move()
+# #     turn_left()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_left()
+# # while not at_goal():
+# #     jump()
+
+# hurdle 3
+# # def turn_right():
+# #     for i in range(0,3):
+# #         turn_left()
+# # def jump():
+# #     turn_left()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_right()
+# #     move()
+# #     turn_left()
+# # while not at_goal():
+# #     if front_is_clear():
+# #         move()
+# #     else:
+# #         jump()
+# hurdle 4
+# # def turn_right():
+# #     for i in range(0,3):
+# #         turn_left()
+# # def jump():
+# #     turn_left()
+# #     while not right_is_clear():
+# #         move()
+# #     turn_right()
+# #     move()
+# #     turn_right()
+# #     while front_is_clear():
+# #         move()
+# #     turn_left()
+# # while not at_goal():
+# #     if front_is_clear():
+# #         move()
+# #     else:
+# #         jump()
+
+# maze
+# # def turn_right():
+# #     for i in range(0,3):
+# #         turn_left()
+# # while not at_goal():
+# #     if right_is_clear():
+# #         turn_right()
+# #         move()
+# #     elif front_is_clear():
+# #         move()
+# #     else:
+# #         turn_left()
