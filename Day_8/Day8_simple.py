7ece9f57c65b8528248b3af5978d0e6380bbf184
KIMHEEJIN <gmlwls768@naver.com>
2022-11-11 00:28:36 +0900

    Day 8 practice

--- Day_8/Day8_simple.py
+++ Day_8/Day8_simple.py
@@ -1,51 +1,63 @@
-# def greet():
-#     print("hi")
-#     print("how do you do")
-#     print(":D")
-# greet()
-# # no parameter func
-# def greet_name(name): #parameter is send by argument
-#     print(f"hi {name}")
-#     print("how do you do")
-#     print("good :D")
-# greet_name("ocean")# argument
-# # parameter func
-
-# def greet_name_location(name, location):
-#     print(f"Hello  {name}")
-#     print(f"What is it like in {location}")
+import os
+import art
+import math
 
 
-# greet_name_location("ocean", "korea")
-# greet_name_location(location="korea", name="ocean")
-# # multiple parameter and keyword argument
-# import math
+def greet():
+    print("hi")
+    print("how do you do")
+    print(":D")
 
 
-# def paint_calc(height, width, cover):
-#     print(f"you need {math.ceil((height * width) / cover)} cans of paint")
+greet()
+# no parameter func
 
 
-# test_h = int(input("Height of wall: "))
-# test_w = int(input("Width of wall: "))
-# coverage = 5
-# paint_calc(height=test_h, width=test_w, cover=coverage)
-# # use parameter func and simple cal
-
-# def prime_checker(number):
-#     is_prime = True
-#     for i in range(2, number):
-#         if (number % i == 0):
-#             is_prime = False
-#     if is_prime:
-#         print("yes")
-#     else:
-#         print("not")
+def greet_name(name):  # parameter is send by argument
+    print(f"hi {name}")
+    print("how do you do")
+    print("good :D")
 
 
-# n = int(input("Check this number: "))
-# prime_checker(number=n)
-# #prime num check
+greet_name("ocean")  # argument
+# parameter func
+
+
+def greet_name_location(name, location):
+    print(f"Hello  {name}")
+    print(f"What is it like in {location}")
+
+
+greet_name_location("ocean", "korea")
+greet_name_location(location="korea", name="ocean")
+# multiple parameter and keyword argument
+
+
+def paint_calc(height, width, cover):
+    print(f"you need {math.ceil((height * width) / cover)} cans of paint")
+
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+# use parameter func and simple cal
+
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if (number % i == 0):
+            is_prime = False
+    if is_prime:
+        print("yes")
+    else:
+        print("not")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
+# prime num check
 
 # def encode(text, shift):
 #     cipher_text = ""
@@ -74,9 +86,6 @@
 # elif direction == "decode":
 #     decode(text, shift)
 
-import art
-import os
-
 
 def caesar(direction, text, shift):
     cipher_text = ""
@@ -94,15 +103,6 @@ def caesar(direction, text, shift):
         else:
             cipher_text += i
     print(f"{direction} is {cipher_text}")
-    # for i in text:
-    #     position = alphabet.index(i)
-    #     new_position = position + shift
-    #     new_position = position + shift
-    #     while new_position >= 26:
-    #         new_position -= 26
-    #     new_letter = alphabet[new_position]
-    #     cipher_text += new_letter
-    # print(f"{direction} is {cipher_text}")
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
@@ -123,3 +123,5 @@ while loop:
     if loop == "No":
         loop = False
         print("Bye~")
+# complete Caesaris code
+# implemented to loop and ignore not alphabet and import logo
