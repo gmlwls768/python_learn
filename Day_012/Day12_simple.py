5f6ba11f5ad9b5f47df3ab300d784740517e8ee7
KIMHEEJIN <gmlwls768@naver.com>
2022-11-15 23:27:10 +0900

    python simple practice

--- Day_012/Day12_simple.py
+++ Day_012/Day12_simple.py
@@ -1,43 +1,44 @@
-# enemies = 1
-
-# def increase_enemies():
-#   enemies = 2
-#   print(f"enemies inside function: {enemies}")
-
-# increase_enemies()
-# print(f"enemies outside function: {enemies}")
-
-# player_health = 2
-# def drink_potion():
-#     potion_strength = 2
-#     print(potion_strength)
-# drink_potion()
-# #local scope global scope
-# def example_increase_enemies():
-#     global enmines
-#     enemies += 1
-#     print(f"enemies inside function: {enemies}")
-#     # ==
-# def example_increase_enemies():
-#     print(f"enemies inside function: {enemies}")
-#     return enemies+1
-# # how to use global scope in function
-
-# #global scope name recommended uppercase
-
-# #Number Guessing Game Objectives:
-
-# # Include an ASCII art logo.
-# # Allow the player to submit a guess for a number between 1 and 100.
-# # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-# # If they got the answer correct, show the actual answer to the player.
-# # Track the number of turns remaining.
-# # If they run out of turns, provide feedback to the player.
-# # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
 import os
-import art
 import random
+import art
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+player_health = 2
+
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+
+drink_potion()
+# local scope global scope
+
+
+def example_increase_enemies():
+    global enmines
+    enemies += 1
+    print(f"enemies inside function: {enemies}")
+    # ==
+
+
+def example_increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies+1
+# how to use global scope in function
+
+# global scope name recommended uppercase
+
+
 EASY_LEVEL = 10
 HARD_LEVEL = 5
 
