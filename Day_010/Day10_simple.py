2c61ada4b35f191278c18536d2b7188e99017d64
KIMHEEJIN <gmlwls768@naver.com>
2022-11-15 20:15:13 +0900

    python simple practice

--- Day_010/Day10_simple.py
+++ Day_010/Day10_simple.py
@@ -1,49 +1,53 @@
+import art
 import os
 
-# def format_name(f_name,l_name):
-#     formated_f_name = f_name.title()
-#     formated_l_name = l_name.title()
-#     return f"{formated_f_name} {formated_l_name}"
-# print(format_name("KIM","HEEJIN"))
-# # use return
 
-# def is_leap(year):
-#     if year % 4 == 0:
-#         if year % 100 == 0:
-#             if year % 400 == 0:
-#                 return True
-#             else:
-#                 return False
-#         else:
-#             return True
-#     else:
-#         return False
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
 
 
-# def days_in_month(year, month):
-#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-#     if month >12 or month<1:
-#         return "Error"
-#     if is_leap(year) == True:
-#         month_days[1] = 29
-#     else:
-#         month_days[1] = 28
-#     return month_days[month-1]
+print(format_name("KIM", "HEEJIN"))
+# use return
 
 
-# # 🚨 Do NOT change any of the code below
-# year = int(input("Enter a year: "))
-# month = int(input("Enter a month: "))
-# days = days_in_month(year, month)
-# print(days)
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
 
-# #leap year use return
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month > 12 or month < 1:
+        return "Error"
+    if is_leap(year) == True:
+        month_days[1] = 29
+    else:
+        month_days[1] = 28
+    return month_days[month-1]
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
+# leap year use return
 
 """ it 
 is
 doc
 string"""
-import art
 
 
 def add(n1, n2):
