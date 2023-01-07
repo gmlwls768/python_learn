f40cafbceb88461a104897a6a3757952c2d434d4
KIMHEEJIN <gmlwls768@naver.com>
2023-01-07 18:07:43 +0900

    python simple practice

--- Day_030/main.py
+++ Day_030/main.py
@@ -1,90 +1,91 @@
-# # Exception Handling
+import pandas
+# Exception Handling
 
-# try:
-#     file = open("Day_030/a_file.txt")
-#     a_dictionary = {"key": "value"}
-#     print(a_dictionary["key"])
-# except FileNotFoundError:
-#     file = open("Day_030/a_file.txt", "w")
-#     file.write("Something")
-# except KeyError as error_message:
-#     print(f"The key {error_message} does not exist.")
-# else:
-#     content = file.read()
-#     print(content)
-# finally:
-#     raise TypeError("This is an error that I made up.")  # custom error
+try:
+    file = open("Day_030/a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("Day_030/a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    # raise TypeError("This is an error that I made up.")  # custom error
+    pass
 
-# # BMI Example
+# BMI Example
 
-# height = float(input("Height: "))
-# weight = int(input("Weight: "))
+height = float(input("Height: "))
+weight = int(input("Weight: "))
 
-# if height > 3:
-#     raise ValueError("Human Height should not be over 3 meters.")
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
 
-# bmi = weight / height ** 2
-# print(bmi)
+bmi = weight / height ** 2
+print(bmi)
 
-# #exercise
+# exercise
 
-# fruits = ["Apple", "Pear", "Orange"]
+fruits = ["Apple", "Pear", "Orange"]
 
-# # TODO: Catch the exception and make sure the code runs without crashing.
-
-
-# def make_pie(index):
-#     try:
-#         fruit = fruits[index]
-#     except IndexError:
-#         fruit = "Fruit"
-#     print(fruit + " pie")
+# TODO: Catch the exception and make sure the code runs without crashing.
 
 
-# make_pie(4)
-
-# facebook_posts = [
-#     {'Likes': 21, 'Comments': 2},
-#     {'Likes': 13, 'Comments': 2, 'Shares': 1},
-#     {'Likes': 33, 'Comments': 8, 'Shares': 3},
-#     {'Comments': 4, 'Shares': 2},
-#     {'Comments': 1, 'Shares': 1},
-#     {'Likes': 19, 'Comments': 3}
-# ]
-
-# total_likes = 0
-
-# for post in facebook_posts:
-#     try:
-#         total_likes = total_likes + post['Likes']
-#     except KeyError:
-#         pass
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        fruit = "Fruit"
+    print(fruit + " pie")
 
 
-# print(total_likes)
+make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        pass
+
+
+print(total_likes)
 
 # Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# import pandas
-
-# data = pandas.read_csv("Day_030/nato_phonetic_alphabet.csv")
-# # TODO 1. Create a dictionary in this format:
-# phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
-
-# # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# {new_key: new_value for (index, row) in df.iterrows()}
 
 
-# def generate():
-#     word = input("Enter a word: ").upper()
-#     try:
-#         output_list = [phonetic_dict[letter] for letter in word]
-#     except KeyError:
-#         print("Sorry, only letters in the alphabet please.")
-#         generate()
-#     else:
-#         print(output_list)
+data = pandas.read_csv("Day_030/nato_phonetic_alphabet.csv")
+# TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 
-# generate()
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate()
+    else:
+        print(output_list)
+
+
+generate()
